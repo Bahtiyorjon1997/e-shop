ede8a8ab4ed1aa0e03e3533b40522e902a72a217
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-04-26 13:51:29 +0200

    react-router-dom installed, routes set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component = { HomePage  } />
+      <Route  path='/hats' component = { Hats } />
     </div>
   );
+}
+
+const Hats = () => {
+  return(
+    <div>Hats</div>
+  )
 }
 
 export default App;
