34f849bcc122c23bbcc26749cda21f696b2dffa6
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-04-28 04:08:33 +0200

    header component created, links to shop and contact pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={ HomePage  } /> 
-      <Route  exact path='/shop' component = { ShopPage } />
-    </Switch>
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
