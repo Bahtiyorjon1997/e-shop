142e78c6e0861a44cf0f3b5751eb5e63ed59161e
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-04-29 04:45:00 +0200

    form-input component built and styled, custom-button component built and styled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-up-and-sign-up.component";
+
 import Header from './components/header/header.component';
+
 
 import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-up-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import "./sign-in-up.component.styles.scss"
+
+class SignInAndSignUp extends React.Component {
+   render(){
+       return <div className="sign-in-and-sign-up"><SignIn/></div>
+   }
+}
+
+export default SignInAndSignUp
